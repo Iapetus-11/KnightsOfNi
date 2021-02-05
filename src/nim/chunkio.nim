87526d0e0f95902f173efcb4eb3a740910b09231
Milo Weinberg <iapetus011@gmail.com>
2021-02-04 22:26:47 -0500

    use let instead of var

--- src/nim/chunkio.nim
+++ src/nim/chunkio.nim
@@ -4,7 +4,7 @@ proc calc_offset(chunk_x: int32, chunk_z: int32): int =
   return 4 * (bitops.bitand(chunk_x, 31) + bitops.bitand(chunk_z, 31) * 32)
 
 proc find_chunk(location: uint32): array[0..1, uint32] =
-  var offset: uint32 = bitops.bitand(bitops.rotateRightBits(location, 8), 0xFFFFFF)
-  var size: uint32 = bitops.bitand(location, 0xFF)
+  let offset: uint32 = bitops.bitand(bitops.rotateRightBits(location, 8), 0xFFFFFF)
+  let size: uint32 = bitops.bitand(location, 0xFF)
 
   return [offset * 4096, size * 4096]
