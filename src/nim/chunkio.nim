3fc6306f3c409f38e9f7fb0609fa988a796f16e8
Milo Weinberg <iapetus011@gmail.com>
2021-02-04 22:36:33 -0500

    update typehints

--- src/nim/chunkio.nim
+++ src/nim/chunkio.nim
@@ -1,6 +1,6 @@
 import bitops
 
-proc calc_offset(chunk_x: int32, chunk_z: int32): int =
+proc calc_offset(chunk_x: int32, chunk_z: int32): int32 =
   return 4 * (bitops.bitand(chunk_x, 31) + bitops.bitand(chunk_z, 31) * 32)
 
 proc find_chunk(location: uint32): array[0..1, uint32] =
