705100faf3e744422719adf561ab0e64f9fbcf67
Milo Weinberg <iapetus011@gmail.com>
2021-02-05 08:45:54 -0500

    do more python code (eww)

--- src/__init__.py
+++ src/__init__.py
@@ -14,6 +14,12 @@ class ChunkIO:
     calc_offset = staticmethod(chunkio.calc_offset)
     find_chunk = staticmethod(chunkio.find_chunk)
 
+    def fetch_chunk(world_path: str, chunk_x: int, chunk_z: int) -> Chunk:
+        chunk_data, timestamp = chunkio.fetch_chunk(world_path, chunk_x, chunk_z)
+
+    async def fetch_chunk_async(world_path: str, chunk_x: int, chunk_z: int) -> Chunk:
+        pass
+
 
 async def setup(server_, config_: dict) -> None:
     global server, config
