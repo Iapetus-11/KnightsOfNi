24acc195a1cebc3a2220cfc0131a5cd5cda04ab9
Milo Weinberg <iapetus011@gmail.com>
2021-02-04 21:39:58 -0500

    add find_chunk() proc

--- src/nim/chunkio.nim
+++ src/nim/chunkio.nim
@@ -2,3 +2,9 @@ import bitops
 
 proc calc_offset(chunk_x: int, chunk_z: int): int =
   return 4 * (bitops.bitand(chunk_x, 31) + bitops.bitand(chunk_z, 31) * 32)
+
+proc find_chunk(location: int): tuple =
+  var offset: int = bitops.bitand(bitops.rotateRightBits(location, 8), 0xFFFFFF)
+  var size: int = bitops.bitand(location, 0xFF)
+
+  return (offset * 4096, size * 4096)
