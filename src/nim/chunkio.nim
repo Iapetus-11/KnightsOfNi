494d87ba177664da256deaa698a8123494234184
Milo Weinberg <iapetus011@gmail.com>
2021-02-04 22:01:14 -0500

    fix find_chunk() proc

--- src/nim/chunkio.nim
+++ src/nim/chunkio.nim
@@ -3,8 +3,10 @@ import bitops
 proc calc_offset(chunk_x: int, chunk_z: int): int =
   return 4 * (bitops.bitand(chunk_x, 31) + bitops.bitand(chunk_z, 31) * 32)
 
-proc find_chunk(location: int): tuple =
-  var offset: int = bitops.bitand(bitops.rotateRightBits(location, 8), 0xFFFFFF)
-  var size: int = bitops.bitand(location, 0xFF)
+proc find_chunk(location: uint32): array[0..1, uint32] =
+  var offset: uint32 = bitops.bitand(bitops.rotateRightBits(location, 8), 0xFFFFFF)
+  var size: uint32 = bitops.bitand(location, 0xFF)
 
-  return (offset * 4096, size * 4096)
+  return [offset * 4096, size * 4096]
+
+echo find_chunk(123)
