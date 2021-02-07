7fa4175bf0eb47085c57d3f88589b00bae2d1144
Milo Weinberg <iapetus011@gmail.com>
2021-02-07 13:50:12 -0500

    add fetch_chunk_async()

--- src/__init__.py
+++ src/__init__.py
@@ -5,6 +5,7 @@ import zlib
 from pymine.api.abc import AbstractChunkIO
 from pymine.types.buffer import Buffer
 from pymine.types.chunk import Chunk
+from pymine.api import call_async
 import pymine.types.nbt as nbt
 
 # KnightsOfNi imports
@@ -15,15 +16,17 @@ class ChunkIO(AbstractChunkIO):
     calc_offset = staticmethod(chunkio.calc_offset)
     find_chunk = staticmethod(chunkio.find_chunk)
 
-    def fetch_chunk(world_path: str, chunk_x: int, chunk_z: int) -> Chunk:
+    @classmethod
+    def fetch_chunk(cls, world_path: str, chunk_x: int, chunk_z: int) -> Chunk:
         chunk_data, timestamp = chunkio.fetch_chunk(world_path, chunk_x, chunk_z)
 
         chunk_data = nbt.TAG_Compound.unpack(Buffer(zlib.decompress(chunk_data)))
 
         return Chunk(chunk_data, timestamp)
 
-    async def fetch_chunk_async(world_path: str, chunk_x: int, chunk_z: int) -> Chunk:
-        pass
+    @classmethod
+    async def fetch_chunk_async(cls, world_path: str, chunk_x: int, chunk_z: int) -> Chunk:
+        return await call_async(cls.fetch_chunk, world_path, chunk_x, chunk_z)
 
 
 async def setup(server, config: dict) -> None:
